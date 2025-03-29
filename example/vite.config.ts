39e4d3d21cf03184fdb05a4e3523092889f34850
joonhoekim <26rote@gmail.com>
2025-03-30 02:59:15 +0900

    Open to network

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,13 +2,15 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/react-pdf-highlighter/",
+  base: "/",
   build: {
     outDir: "dist",
   },
   plugins: [react()],
   server: {
+    host: "0.0.0.0",
     port: 3003,
+    strictPort: true,
   },
   define: {
     APP_VERSION: JSON.stringify(process.env.npm_package_version),
